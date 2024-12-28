defd2f4d7883f77f14ec9e2faf23021226352b0f
Vinay D <vinayhari789@gmail.com>
2024-12-28 17:19:22 +0530

    free session template

--- lead-frontend/src/components/main/admin/LeadDetails.jsx
+++ lead-frontend/src/components/main/admin/LeadDetails.jsx
@@ -64,7 +64,7 @@ const LeadDetails = ({ selectedImage, templateName, templateLanguage }) => {
       websiteLink: 'https://lara.co.in',
     };
 
-    if(templateName === 'lara_jan2025_batch'){
+    if(templateName === 'lara_jan2025_batch' || templateName === 'sunday_logical_reasoning_free_sessions'){
       const laraTemplatepayload = {
         to,
         templateName: templateName, 
